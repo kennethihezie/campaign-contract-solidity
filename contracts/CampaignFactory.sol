f7ecf12dee9e403a018551de95b454d8fe773a44
kennethihezie <kennethihezie06@gmail.com>
2023-12-01 05:15:27 +0100

    changes

--- contracts/CampaignFactory.sol
+++ contracts/CampaignFactory.sol
@@ -3,17 +3,17 @@ pragma solidity >= 0.4.22 <0.9.0;
 import './Campaign.sol';
 
 contract CampaignFactory {
-  address[] private deployedContracts;
+  address payable[] private deployedContracts;
 
   function createCampaign(uint minimumContribution) public returns(address) {
       // This deploy the campaign contract on the blockchain
       address newCampaignAddress = address(new Campaign(msg.sender, minimumContribution));
-      deployedContracts.push(newCampaignAddress);
+      deployedContracts.push(payable(newCampaignAddress));
 
       return address(newCampaignAddress);
   }
 
-  function getAllDeployedContracts() public view returns(address[] memory) {
+  function getAllDeployedContracts() public view returns(address payable[] memory) {
     return deployedContracts;
   }
 }
